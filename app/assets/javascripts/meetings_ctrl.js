9820ea8448f0c54ad0249f0c02b94e2287a68225
Brian <riceb53@gmail.com>
2016-12-31 11:31:26 -0500

    Update

---
+++ app/assets/javascripts/meetings_ctrl.js
@@ -0,0 +1,8 @@
+(function() {
+  "use strict";
+
+  angular.module("app").controller("meetingsCtrl",function($scope, $http){
+    $scope.message = 'hello';
+  });
+
+}());
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@ Rails.application.routes.draw do
   get '/' => 'meetings#index'
 
   get '/meetings' => 'meetings#index'
+  get '/meetings/new' => 'meetings#new'
+  post '/meetings' => 'meetings#create'
   get '/meetings/:id' => 'meetings#show'
+
+  namespace :api do
+    namespace :v1 do
+      get '/meetings' => 'meetings#index'
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
